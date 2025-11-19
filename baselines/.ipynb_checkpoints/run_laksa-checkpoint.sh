81f3939f6526f78ffb01c23b78acc61e913e47c1
root <root@g4c6a33233ef415487955f678a4e4062-task0-0.g4c6a33233ef415487955f678a4e4062.b4a929e0d842479ab28033a36398315a.svc.cluster.local>
2025-11-19 17:12:05 +0800

    evaluation modify 2025119 v1

--- baselines/.ipynb_checkpoints/run_laksa-checkpoint.sh
+++ baselines/.ipynb_checkpoints/run_laksa-checkpoint.sh
@@ -1,10 +1,10 @@
 MODE="RAG"    # CoT/Direct/RAG/Logical
-DATASET_NAME="gsm8k"
+DATASET_NAME="ProofWriter"
 MODEL_NAME="qwen7"
 SPLIT="test"
-LANGCHAIN_DB="logicaldeduction"
+LANGCHAIN_DB="gsm8k"
 RAG_TOPK=10
-DEMONSTRATION_NUM=2
+DEMONSTRATION_NUM=1
 ZERO_SHOT=true
 
 RUN_CMD="python llms_baseline.py --model_name $MODEL_NAME --dataset_name $DATASET_NAME --split $SPLIT --mode $MODE --max_new_tokens 8192 --db_name $LANGCHAIN_DB --icl_num $DEMONSTRATION_NUM --top_k $RAG_TOPK --batch_test --batch_size 16 --use_vllm --all_data_switch"
